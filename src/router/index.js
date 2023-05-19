96809dabd69469a40d19fd97aa4a46040d5f92ad
LOUMON Koffi Samuel <lkoffisamuel@gmail.com>
2023-05-19 12:43:13 +0000

    add page router

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,41 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import DetailView from "../views/DetailView.vue";
+import SearchView from "../views/SearchView.vue";
+import UpcomingView from '../views/UpcomingView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/genre/:genre",
+      name: "genre",
+      props: true,
+      component: HomeView,
+    },
+    {
+      path: "/upcoming",
+      name: "upcoming",
+      component: UpcomingView,
+    },
+    {
+      path: "/detail/:media/:id",
+      name: "detail",
+      props: true,
+      component: DetailView,
+    },
+    {
+      path: "/search/:search",
+      name: "search",
+      props: true,
+      component: SearchView,
+    },
+  ],
+});
 
-export default router
+export default router;
